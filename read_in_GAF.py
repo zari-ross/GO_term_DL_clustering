69d526686d39665c9c7f72d8d30a6cba6a4e4090
Nataliya Trushina <nitrushina@hotmail.com>
2023-04-20 16:48:11 +0200

    Select GO-terms that only appear in Rat database. Only select names for molecular functionh, clean the names and save as json file.

--- read_in_GAF.py
+++ read_in_GAF.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import pronto
+import json
+
+from Project.Project_GO_term_cleaning_names import clean_name
 
 # Define column names for the GAF file
 column_names = [
@@ -26,4 +30,40 @@ file_path = "rgd.gaf"
 gaf_df = pd.read_csv(file_path, sep="\t", comment="!", names=column_names, skiprows=12)
 
 # Print the first few rows of the DataFrame
-print(gaf_df.head())
+# print(gaf_df.head())
+
+# Extract unique GO terms from the DataFrame
+unique_go_terms = gaf_df['GO_ID'].unique()
+
+# Convert the result to a list
+unique_go_terms_list = list(unique_go_terms)
+
+# Print the list of unique GO terms
+# print(unique_go_terms_list)
+
+go_ids = unique_go_terms_list
+
+# Load OBO file
+file_path = "go-basic.obo"
+ontology = pronto.Ontology(file_path)
+
+# # Get term names for the GO IDs in your experiment
+# term_names = [ontology[go_id].name for go_id in go_ids if go_id in ontology]
+
+# Get term names for the GO IDs in your experiment with namespace "molecular_function"
+term_names = [ontology[go_id].name for go_id in go_ids if
+              go_id in ontology and ontology[go_id].namespace == "molecular_function"]
+
+filtered_names = [name for name in term_names if "obsolete" not in name.lower() and "unknown" not in name.lower() and
+                  "uncharacterized" not in name.lower()]
+
+# Clean all filtered names
+cleaned_names = [clean_name(name) for name in filtered_names]
+
+# print(cleaned_names)
+# print(len(cleaned_names))
+
+output_file = "cleaned_names_MF_rat.json"
+
+with open(output_file, "w") as f:
+    json.dump(cleaned_names, f)
