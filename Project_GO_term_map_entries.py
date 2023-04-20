0332e2f8aecd01dbe7afd3db1133f99b3ad49571
Nataliya Trushina <nitrushina@hotmail.com>
2023-04-20 11:39:24 +0200

    Matching keys to clusters.

--- Project_GO_term_map_entries.py
+++ Project_GO_term_map_entries.py
@@ -10,6 +10,7 @@ from nltk.stem import WordNetLemmatizer
 import string
 from Project_GO_term_cleaning_names import clean_name
 from gensim.models import Word2Vec
+from collections import Counter
 
 nltk.download('stopwords')
 nltk.download('wordnet')
@@ -31,24 +32,24 @@ cluster_representatives = data['cluster_representatives']
 selected_words = random.sample(words, 150)
 selected_indices = [words.index(word) for word in selected_words]
 
-# Plot the 2D embeddings for all words
-plt.figure(figsize=(10, 10))
-plt.scatter(embeddings_2d[:, 0], embeddings_2d[:, 1], c=clusters, cmap='viridis')
-
-# Add labels for the selected words
-for i in selected_indices:
-    x, y = embeddings_2d[i, :]
-    plt.annotate(words[i], xy=(x, y), xytext=(5, 2), textcoords='offset points', ha='right', va='bottom')
-
-# Add cluster representative labels
-for cluster_id, cluster_word in cluster_representatives.items():
-    cluster_centroid = np.mean(embeddings_2d[clusters == cluster_id], axis=0)
-    plt.annotate(cluster_word, xy=(cluster_centroid[0], cluster_centroid[1]), xytext=(-15, 15),
-                 textcoords='offset points',
-                 ha='center', va='center', fontsize=12, color='red',
-                 bbox=dict(boxstyle='round, pad=0.5', edgecolor='red', facecolor='white', alpha=0.7))
-
-plt.show()
+# # Plot the 2D embeddings for all words
+# plt.figure(figsize=(10, 10))
+# plt.scatter(embeddings_2d[:, 0], embeddings_2d[:, 1], c=clusters, cmap='viridis')
+#
+# # Add labels for the selected words
+# for i in selected_indices:
+#     x, y = embeddings_2d[i, :]
+#     plt.annotate(words[i], xy=(x, y), xytext=(5, 2), textcoords='offset points', ha='right', va='bottom')
+#
+# # Add cluster representative labels
+# for cluster_id, cluster_word in cluster_representatives.items():
+#     cluster_centroid = np.mean(embeddings_2d[clusters == cluster_id], axis=0)
+#     plt.annotate(cluster_word, xy=(cluster_centroid[0], cluster_centroid[1]), xytext=(-15, 15),
+#                  textcoords='offset points',
+#                  ha='center', va='center', fontsize=12, color='red',
+#                  bbox=dict(boxstyle='round, pad=0.5', edgecolor='red', facecolor='white', alpha=0.7))
+#
+# plt.show()
 
 filename = "GO_terms_Example1.txt"
 
@@ -88,12 +89,31 @@ for name in tokenized_cleaned_names:
 
 print(experiment_word_embeddings)
 
-# # 5. Calculate the enrichment in the clusters
-# # One way is to calculate the frequency of each cluster in the experiment
-# cluster_frequencies = np.bincount(experiment_clusters)
-# total_clusters = len(cluster_frequencies)
-#
-# # Print the enrichment results
-# for cluster_id, freq in enumerate(cluster_frequencies):
-#     enrichment = freq / total_clusters
-#     print(f"Enrichment of cluster {cluster_id}: {enrichment}")
+
+# Get the 2D embeddings for the experiment words
+experiment_word_2d_embeddings = [embeddings_2d[words.index(word)] for word in experiment_word_embeddings.keys()]
+
+# Predict the cluster assignments for the experiment words
+experiment_word_clusters = kmeans.predict(experiment_word_2d_embeddings)
+
+# Count the number of experiment words in each cluster
+experiment_word_counts = Counter(experiment_word_clusters)
+
+# Count the number of all words in each cluster
+all_word_counts = Counter(clusters)
+
+# Calculate the enrichment score for each cluster
+enrichment_scores = {}
+for cluster_id in range(num_clusters):
+    experiment_prop = experiment_word_counts[cluster_id] / len(experiment_word_embeddings)
+    all_prop = all_word_counts[cluster_id] / len(words)
+    enrichment_scores[cluster_id] = experiment_prop / all_prop
+
+print(enrichment_scores)
+
+# Plot the enrichment scores
+plt.bar(range(num_clusters), [enrichment_scores[i] for i in range(num_clusters)])
+plt.xlabel("Cluster ID")
+plt.ylabel("Enrichment Score")
+plt.title("Cluster Enrichment")
+plt.show(): {enrichment}")
